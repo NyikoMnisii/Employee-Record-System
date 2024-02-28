c27003fb6a75bf85709c75eaa462079b9ad3b77b
Nyiko Mnisi <94037118+Nyikotboy@users.noreply.github.com>
2024-02-28 12:40:00 +0200

    ADD EMPLOYEE UI DONE

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://username:password@localhost/EmployeeDB'  # Replace with your MySQL connection string
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://EmployeeDB'  # Replace with your MySQL connection string
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
